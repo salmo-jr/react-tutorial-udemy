8d4d75e1ad93cb5cce8c93b1f5dc93f24bbfbf74
salmo-jr <salmo.jr@gmail.com>
2020-08-03 22:54:21 -0300

    lesson 72 completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Person from './Person/Person';
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.alt ? 'red' : 'green'};
   color: white;
   font: inherit;
   border: 1px solid blue;
@@ -12,7 +12,7 @@ const StyledButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: lightgreen;
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
     color: black;
   }
 `;
@@ -102,6 +102,7 @@ const app = props => {
         <h1>Hi, I'm a React App.</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <StyledButton
+          alt={personsVisibility.showPersons}
           onClick={tooglePersonsHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
