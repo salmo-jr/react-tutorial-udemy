6aea42945dfcf598b27a5abbb7866afeecd828c4
salmo-jr <salmo.jr@gmail.com>
2020-08-21 10:45:54 -0300

    lesson 92 completed

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,6 +32,15 @@ class App extends Component {
       console.log('[App.js] componentDidMount');
     }
 
+    componentDidUpdate(){
+      console.log('[App.js] componentDidUpdate');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+      console.log('[App.js] shouldComponentUpdate');
+      return true;
+    }
+
     nameChangedHandler = (event, id) => {
       const personIndex = this.state.persons.findIndex(p => {
         return p.id === id;
