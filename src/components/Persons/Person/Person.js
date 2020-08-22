4269f10da4c2731dec7702f02e3b75666808516b
salmo-jr <salmo.jr@gmail.com>
2020-08-22 11:15:07 -0300

    lesson 105 - Higher Order Components (HOC) - introduction

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
@@ -6,11 +6,11 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering ...');
         return (
-            <Fragment>
+            <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name}! And I'm {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
                 <input type='text' onChange={this.props.changed} value={this.props.name} />
-            </Fragment>
+            </Auxiliary>
         );
     }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props){
@@ -80,7 +81,7 @@ class App extends Component {
 
       // PREFER TO USE BIND THAN ARROW FUNCTION
       return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button
             onClick={() => {
               this.setState({showCockpit: false});
@@ -95,7 +96,7 @@ class App extends Component {
             clicked={this.tooglePersonsHandler}/>
             : null }
           {persons}
-        </div>
+        </WithClass>
       );
     }
     //return React.createElement('div',{className:"App"}, 'text');
