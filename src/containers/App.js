1d398d948ab2fafb2efbd44ac196216f187524e7
salmo-jr <salmo.jr@gmail.com>
2020-08-22 12:52:53 -0300

    lesson 108 - Setting State Correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,8 @@ class App extends Component {
       ],
       otherState: 'some other value',
       showPersons: false,
-      showCockpit: true
+      showCockpit: true,
+      changeCounter: 0
     }
 
     static getDerivedStateFromProps(props, state){
@@ -55,7 +56,12 @@ class App extends Component {
       const newPersons = [...this.state.persons];
       newPersons[personIndex] = person;
 
-      this.setState({ persons: newPersons });
+      this.setState((prevState, props) => {
+        return {
+          persons: newPersons,
+          changeCounter: prevState.changeCounter + 1
+        };
+      });
     };
 
     deletePersonHandler = (personIndex) => {
